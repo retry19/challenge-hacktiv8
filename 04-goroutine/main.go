2f75b2c036d7183201e0ce65f28eccfb1cea3695
Reza Rachmanuddin <rezarahmanudin@gmail.com>
2024-10-16 17:38:15 +0700

    feat: add 04

---
+++ 04-goroutine/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	// WithLocking()
+	WithoutLocking()
+}
+
+func WithLocking() {
+	firstData := []interface{}{
+		"1",
+		"2",
+		"3",
+		"4",
+	}
+
+	secondData := []interface{}{
+		"1",
+		"2",
+		"3",
+		"4",
+	}
+
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
+	for _, v := range firstData {
+		wg.Add(1)
+		go func(v interface{}) {
+			mu.Lock()
+			fmt.Println(v)
+			mu.Unlock()
+			wg.Done()
+		}(v)
+	}
+
+	for _, v := range secondData {
+		wg.Add(1)
+		go func(v interface{}) {
+			mu.Lock()
+			fmt.Println(v)
+			mu.Unlock()
+			wg.Done()
+		}(v)
+	}
+
+	wg.Wait()
+}
+
+func WithoutLocking() {
+	firstData := []interface{}{
+		"1",
+		"2",
+		"3",
+		"4",
+	}
+
+	secondData := []interface{}{
+		"1",
+		"2",
+		"3",
+		"4",
+	}
+
+	var wg sync.WaitGroup
+
+	for _, v := range firstData {
+		wg.Add(1)
+		go func(v interface{}) {
+			fmt.Println(v)
+			wg.Done()
+		}(v)
+	}
+
+	for _, v := range secondData {
+		wg.Add(1)
+		go func(v interface{}) {
+			fmt.Println(v)
+			wg.Done()
+		}(v)
+	}
+
+	wg.Wait()
+}
